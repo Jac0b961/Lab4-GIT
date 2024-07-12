58d513d5fd898b7ed75021b62328f6f0ecd10cd9
Jac0b961 <opona9611@gmail.com>
2024-07-12 20:54:24 +0200

    Add single dash options

--- skrypt.sh
+++ skrypt.sh
@@ -1,28 +1,26 @@
 #!/bin/bash
 
-#!/bin/bash
-
 case "$1" in
-    --date)
+    --date|-d)
         date
         ;;
-    --logs)
+    --logs|-l)
         num=${2:-100}
         for i in $(seq 1 $num); do
             echo "log$i.txt created by skrypt.sh on $(date)" > log$i.txt
         done
         ;;
-    --help)
+    --help|-h)
         echo "Dostepne opcje:"
-        echo "--date          Wyswietla dzisiejsza data"
-        echo "--logs [NUM]    Tworzy NUM plikow logx.txt (domyslnie 100)"
+        echo "--date -d       Wyswietla dzisiejsza date"
+        echo "--logs -l [NUM] Tworzy NUM plikow logx.txt (domyslnie 100)"
         echo "--init          Klonuje repozytorium"
-        echo "--error [NUM]   Tworzy NUM plikow errorx/errorx.txt (domyslnie 100)"
+        echo "--error -e [NUM] Tworzy NUM plikow errorx/errorx.txt (domyslnie 100)"
         ;;
     --init)
         git clone https://github.com/Jac0b961/Lab4-GIT.git
         ;;
-    --error)
+    --error|-e)
         num=${2:-100}
         mkdir -p error
         for i in $(seq 1 $num); do
