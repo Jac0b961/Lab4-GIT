58fd6b18ea1cce7f7f65532fdaaab19304b88b01
Jac0b961 <opona9611@gmail.com>
2024-07-12 20:34:05 +0200

    Add --logs option to script

--- skrypt.sh
+++ skrypt.sh
@@ -4,7 +4,13 @@ case "$1" in
     --date)
         date
         ;;
+    --logs)
+        num=${2:-100}
+        for i in $(seq 1 $num); do
+            echo "log$i.txt created by skrypt.sh on $(date)" > log$i.txt
+        done
+        ;;
     *)
-        echo "Opcja nieznana. Uzyj --date"
+        echo "Opcja nieznana. Uzyj --date lub --logs"
         ;;
 esac
