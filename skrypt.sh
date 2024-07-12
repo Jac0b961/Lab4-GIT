102a70654097a77332ba35c946337cdd487b0de9
Jac0b961 <opona9611@gmail.com>
2024-07-12 20:44:10 +0200

    Add --error option to script

--- skrypt.sh
+++ skrypt.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#!/bin/bash
+
 case "$1" in
     --date)
         date
@@ -12,12 +14,20 @@ case "$1" in
         ;;
     --help)
         echo "Dostepne opcje:"
-        echo "--date          Wyswietla dzisiejsza date"
+        echo "--date          Wyswietla dzisiejsza data"
         echo "--logs [NUM]    Tworzy NUM plikow logx.txt (domyslnie 100)"
         echo "--init          Klonuje repozytorium"
+        echo "--error [NUM]   Tworzy NUM plikow errorx/errorx.txt (domyslnie 100)"
         ;;
     --init)
         git clone https://github.com/Jac0b961/Lab4-GIT.git
+        ;;
+    --error)
+        num=${2:-100}
+        mkdir -p error
+        for i in $(seq 1 $num); do
+            echo "error$i.txt created by skrypt.sh on $(date)" > error/error$i.txt
+        done
         ;;
     *)
         echo "Opcja nieznana. Uzyj --help, aby zobaczyc dostepne opcje"
